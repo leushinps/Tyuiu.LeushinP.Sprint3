d37d0098ab4a6f4835fc2a693a9ac37f7299624a
Pavel Leushin <leushinps@outlook.com>
2025-11-15 01:45:57 +0500

    Task2

---
+++ Tyuiu.LeushinP.Sprint3.Task.2.V6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Tyuiu.LeushinP.Sprint3.Task2.V6.Lib;
+
+namespace Tyuiu.LeushinP.Sprint3.Task2.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Леушин П.С | Смартб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла dо while                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Леушин Павел Сергеевич | Смартб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do while, которая вычисляет           *");
+            Console.WriteLine("* произведение ряда по формуле, при a=0,25                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double a = 0.25;
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"startValue = {startValue}");
+            Console.WriteLine($"stopValue = {stopValue}");
+
+            double result = ds.GetMultiplySeries(a, startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Произведение ряда = {result:F6}");
+
+            Console.WriteLine("Для завершения нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
