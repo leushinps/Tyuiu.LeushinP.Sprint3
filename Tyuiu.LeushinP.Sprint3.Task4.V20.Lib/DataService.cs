d442abe902ff571e359f14396e54e605cf17656e
Pavel Leushin <leushinps@outlook.com>
2025-11-27 00:22:17 +0500

    Task4

--- Tyuiu.LeushinP.Sprint3.Task4.V20.Lib/DataService.cs
+++ Tyuiu.LeushinP.Sprint3.Task4.V20.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.LeushinP.Sprint3.Task4.V20.Lib
         public double Calculate(int startValue, int stopValue)
         {
             double res = 1.0;
-
+            
             for (int x = startValue; x <= stopValue; x++)
             {
                 if (x == 0) continue;
