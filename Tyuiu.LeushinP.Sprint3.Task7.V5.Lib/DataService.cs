1e857fe1aca46b70e0302d5f6ec665adcf5d1c62
Pavel Leushin <leushinps@outlook.com>
2025-11-27 00:50:18 +0500

    Task7

--- Tyuiu.LeushinP.Sprint3.Task7.V5.Lib/DataService.cs
+++ Tyuiu.LeushinP.Sprint3.Task7.V5.Lib/DataService.cs
@@ -7,24 +7,18 @@ namespace Tyuiu.LeushinP.Sprint3.Task7.V5.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] valueArray = new double[stopValue - startValue + 1];
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
             double y;
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                if (3 * x + 1.5 == 0)
-                {
-                    valueArray[count] = 0;
-                    count++;
-                    continue;
-                }
-                y = ((2 * Math.Sin(x)) / (3 * x + 1.2)) + Math.Cos(x) - 7 * x * 2;
-                y = Math.Round(y, 2);
+                y = Math.Round(((2 * Math.Sin(x)) / (3 * x + 1.2)) + Math.Cos(x) - 7 * x * 2, 2);
                 valueArray[count] = y;
                 count++;
             }
             return valueArray;
-
         }
     }
 }
